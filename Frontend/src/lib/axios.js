deebc37cce9be0d8fc431d61c3be4f132505ee67
jasimahamed <jasim2003ahamed@gmail.com>
2025-06-07 21:58:04 +0530

    updated

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : 'https://backend-q3iq.onrender.com'
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : 'https://backend-q3iq.onrender.com/api'
   ,
   withCredentials: true,
 });
