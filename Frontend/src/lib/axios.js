291512416b07173c577fa190f145c7679c42ac2c
jasimahamed <jasim2003ahamed@gmail.com>
2025-06-07 19:40:15 +0530

    updated

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : 'https://backend-q3iq.onrender.com'
+  ,
   withCredentials: true,
 });
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -141,7 +141,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000" : "https://backend-q3iq.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
